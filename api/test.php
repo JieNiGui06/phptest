cc50058923ed3efb2ea988d15a953d650da3fb5f
杰尼龟 <329125460@qq.com>
2023-01-14 10:07:14 +0800

    Update test.php

--- api/test.php
+++ api/test.php
@@ -5,9 +5,18 @@
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
    
-}
-else{
-    echo $_GET["a"];
+} else {
+    //echo $_GET["a"];
+    if ($_GET["t"] == "w") {
+        $myfile = fopen("test.txt", "w") or die("Unable to open file!");
+        fwrite($myfile, $_GET["content"]);
+        fclose($myfile);
+        echo "ok.";
+    } else {
+        $myfile = fopen("test.txt", "r") or die("Unable to open file!");
+        echo fread($myfile, filesize("test.txt"));
+        fclose($myfile);
+    }
 }
 
 function test_input($data) {
