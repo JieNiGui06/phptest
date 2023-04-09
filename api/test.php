465af900f2c92cc0e15694e8bcea6a9f44179dea
杰尼龟 <329125460@qq.com>
2023-04-09 09:43:51 +0800

    Update test.php

--- api/test.php
+++ api/test.php
@@ -7,7 +7,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
    
 }
 else{
-    echo $_GET["a"];
+    //echo $_GET["a"];
+   $url='https://www.php.cn/php-weizijiaocheng-391093.html';
+$html = file_get_contents($url);
+echo $html;
 }
 
 function test_input($data) {
